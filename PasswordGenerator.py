d53ba6af71545115c01f7f702bff8a69e4000d60
Moe <56777764+iMicro42@users.noreply.github.com>
2019-10-19 22:15:34 -0400

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,12 @@
+import random
+chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?><:|{}+_~-=[].,/';'"
+
+passwordOut = []
+
+for x in range(20):
+    y = random.randint(0,65)
+    passwordOut.append(chars[y])
+
+x = ''.join(passwordOut)
+
+print(x)
